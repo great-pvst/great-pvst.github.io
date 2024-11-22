b2ef8ec321e220302a533d27f357046f8cc7c2fe
great-pvst <176429168+great-pvst@users.noreply.github.com>
2024-11-22 16:51:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,25 @@
-// Récupérer les éléments nécessaires
+const buttons = document.querySelectorAll("[data-popup]");
+const closeButtons = document.querySelectorAll("[data-close]");
 
-const openPopupBtn = document.getElementById("openPopupBtn");
-const closePopupBtn = document.getElementById("closePopupBtn");
-const popup = document.getElementById("popup");
+function openPopup(popupId) {
+    const popup = document.getElementById(popupId);
+    if (popup) {
+      popup.style.display = "flex";}}
 
-const video = document.getElementById('myVideo');
-const playPauseBtn = document.getElementById('playPauseBtn');
-const muteBtn = document.getElementById('muteBtn');
+function closePopups() {
+    document.querySelectorAll(".popup").forEach(popup => {
+      popup.style.display = "none";});}
 
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+    const popupId = button.getAttribute("data-popup");
+    openPopup(popupId);  });});
 
-// Afficher le popup lorsque le bouton est cliqué
-openPopupBtn.addEventListener("click", function() {
-    popup.style.display = "flex";
-});
+closeButtons.forEach(button => {
+    button.addEventListener("click", closePopups);});
 
-// Fermer le popup lorsque l'utilisateur clique sur la croix
-closePopupBtn.addEventListener("click", function() {
-    popup.style.display = "none";
-});
-
-// Fermer le popup si l'utilisateur clique en dehors du contenu
-window.addEventListener("click", function(event) {
-    if (event.target === popup) {
-        popup.style.display = "none";
+window.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      closePopups();
     }
 });
-
-
-//video
-// Contrôle Lecture/Pause
-playPauseBtn.addEventListener('click', () => {
-  if (video.paused) {
-    video.play();
-    playPauseBtn.textContent = 'Pause';
-  } else {
-    video.pause();
-    playPauseBtn.textContent = 'Lire';
-  }
-});
-
-// Contrôle du Son
-muteBtn.addEventListener('click', () => {
-  video.muted = !video.muted;
-  muteBtn.textContent = video.muted ? 'Réactiver le son' : 'Couper le son';
-});
-
